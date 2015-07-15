474294fa7a06ad94fd241f8762c6faec49c73041
Jeff Mace <jeffmace@gmail.com>
2015-07-15 16:53:50 -0400

    Make sure the /etc/hosts file is created correctly in Virtualbox when adding a domain to the hostname Cleanup the example files for vcenter Use the 1.1.0 version of the puppet module

--- modules/continuent_vagrant/manifests/hosts.pp
+++ modules/continuent_vagrant/manifests/hosts.pp
@@ -17,14 +17,30 @@
 class continuent_vagrant::hosts {
 	if $ec2_instance_id == "" {
 	  if $platform == 'virtualbox'  or $platform == '' {
-			host { "db1": ip => "192.168.11.101", }
-			host { "db2": ip => "192.168.11.102", }
-			host { "db3": ip => "192.168.11.103", }
-			host { "db4": ip => "192.168.11.104", }
-			host { "db5": ip => "192.168.11.105", }
-			host { "db6": ip => "192.168.11.106", }
-			host { "db7": ip => "192.168.11.107", }
-			host { "db8": ip => "192.168.11.108", }
+	    if $vagrant_hosts_domain != '' {
+	      $additional_domain = $vagrant_hosts_domain
+	    } else {
+	      $additional_domain = "$domain"
+	    }
+	    if $additional_domain == '' {
+	      host { "db1": ip => "192.168.11.101", }
+  			host { "db2": ip => "192.168.11.102", }
+  			host { "db3": ip => "192.168.11.103", }
+  			host { "db4": ip => "192.168.11.104", }
+  			host { "db5": ip => "192.168.11.105", }
+  			host { "db6": ip => "192.168.11.106", }
+  			host { "db7": ip => "192.168.11.107", }
+  			host { "db8": ip => "192.168.11.108", }
+	    } else {
+	      host { "db1.${additional_domain}": ip => "192.168.11.101", host_aliases => "db1"}
+  			host { "db2.${additional_domain}": ip => "192.168.11.102", host_aliases => "db2"}
+  			host { "db3.${additional_domain}": ip => "192.168.11.103", host_aliases => "db3"}
+  			host { "db4.${additional_domain}": ip => "192.168.11.104", host_aliases => "db4"}
+  			host { "db5.${additional_domain}": ip => "192.168.11.105", host_aliases => "db5"}
+  			host { "db6.${additional_domain}": ip => "192.168.11.106", host_aliases => "db6"}
+  			host { "db7.${additional_domain}": ip => "192.168.11.107", host_aliases => "db7"}
+  			host { "db8.${additional_domain}": ip => "192.168.11.108", host_aliases => "db8"}
+	    }
 		}
 	} else {
     #Openstack hosts have the ec2 facts loaded but
